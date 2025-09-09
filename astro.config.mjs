a2712ce8a225ad7a9bbae002d7b091c7fb549492
Ignacio Shishko <shishkoignacio@gmail.com>
2025-09-09 00:39:09 -0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	// Ruta de despliegue para GitHub Pages
+	site: "https://ishishko.github.io",
+	base: "/corralplast/",
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
